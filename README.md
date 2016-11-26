64315c3909b221319fa99fc13f4d965b08e215c5
abhas saroha <abhassaroha@gmail.com>
2016-11-26 22:46:18 +0530

    added a dummy word count processor for pyspark

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # twitter-sentiment
 A toy project to do sentiment analysis on twitter streams
+
+### Dependencies
+* Create application keys for twitter api
+* Install kafka
+* Install apache spark
+* Install pyspark
+* A browser which supports websocket
---
+++ stream-processor/process.py
@@ -0,0 +1,26 @@
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+count = 0
+ssc = None
+
+def terminate(rdd):
+    global count
+    count = count + 1
+    if count == 10:
+        ssc.stop()
+
+def run(sc):
+    global ssc
+    ssc = StreamingContext(sc, 1)
+    directKafkaStream = KafkaUtils.createDirectStream(ssc, ["twitter"],
+            {"metadata.broker.list":"localhost:9092", 
+                "auto.offset.reset": "smallest"}) 
+    lines = directKafkaStream.map(lambda tuple: tuple[1]) 
+    words = lines.flatMap(lambda line: line.split())
+    pairs = words.map(lambda word: (word, 1))
+    wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+    wordCounts.pprint()
+    directKafkaStream.foreachRDD(terminate)
+    ssc.start()
+    ssc.awaitTermination()
+
